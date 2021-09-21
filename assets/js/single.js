196889133d4bbecc17ef4bde5530c1fba7609c6e
Kelbach <87092340+Kelbach@users.noreply.github.com>
2021-09-21 14:08:18 -0400

    6.3.5.2 it works

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,33 @@
-var displayIssues = function(issues) {
+var issueContainerEl = document.querySelector("#issues-container");
 
+var displayIssues = function(issues) {
+    for (var i = 0; i < issues.length; i++) {
+        //create link element to issues
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        issueEl.appendChild(titleEl);
+
+        var typeEl = document.createElement("span");
+
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)";
+        } else {
+            typeEl.textContent = "(Issue)";
+        }
+
+        if (issues.length === 0) {
+            issueContainerEl.textContent = "This repo has no open issues.";
+            return;
+        }
+
+        issueEl.appendChild(typeEl);
+        issueContainerEl.appendChild(issueEl);
+    }
 };
 
 var getRepoIssues = function(repo) {
@@ -7,13 +35,12 @@ var getRepoIssues = function(repo) {
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data){
-                console.log(data);
+                displayIssues(data);
             });
         } else {
             alert("There was a problem with your request.");
         }
     });
-    console.log(repo);
 };
 
 getRepoIssues("facebook/react");
