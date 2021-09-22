183eb16b6c760328c92101cde0a59ff23335aa94
Kelbach <87092340+Kelbach@users.noreply.github.com>
2021-09-22 15:26:22 -0400

    6.5.7 buttons working?

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,16 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    
+    if (language) {
+        getFeaturedRepos(language);
+        repoContainerEl.textContent = "";
+    }
+}
 
 var getFeaturedRepos = function(language) {
     var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
@@ -95,3 +105,4 @@ var getUserRepos = function(user) {
     });
 };
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
